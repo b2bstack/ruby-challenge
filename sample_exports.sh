606e56576b49625f98dce974f53a9b23eb90eeb2
JesusGautamah <lima.jesuscc@gmail.com>
2022-03-29 13:29:12 -0300

    feat: Database configuration with enviroment variables. Edit and run sample_exports.sh

---
+++ sample_exports.sh
@@ -0,0 +1,12 @@
+# user: <%= ENV["POSTGRES_USER"] %>
+# password: <%= ENV["POSTGRES_PASSWORD"] %>
+# host: <%= ENV["POSTGRES_HOST"] %>
+# port: <%= ENV["POSTGRES_PORT"] %>
+# template: <%= ENV["POSTGRES_TEMPLATE"] %>
+
+export POSTGRES_USER=
+export POSTGRES_PASSWORD=
+export POSTGRES_HOST=
+export POSTGRES_PORT=
+export POSTGRES_TEMPLATE=
+

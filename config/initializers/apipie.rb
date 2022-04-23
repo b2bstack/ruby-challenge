3051271eca9f40431dd703fdf1edf533f6d5728a
darlan.dieterich <darlandieterich@gmail.com>
2022-04-23 14:21:11 -0300

    fix: update route api documentation

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "RubyChallenge"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/documentation"
+  config.doc_base_url            = "api/doc"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
